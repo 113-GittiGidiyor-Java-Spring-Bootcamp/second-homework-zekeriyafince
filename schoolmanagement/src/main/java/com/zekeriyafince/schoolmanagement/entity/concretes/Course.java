e433ed9aee390ebe2993e1630aa1f6badaebf9fa
zekeriyafince <zekeriyafurkanince@gmail.com>
2021-08-17 00:00:55 +0300

    adding entity model

---
+++ schoolmanagement/src/main/java/com/zekeriyafince/schoolmanagement/entity/concretes/Course.java
@@ -0,0 +1,40 @@
+package com.zekeriyafince.schoolmanagement.entity.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.zekeriyafince.schoolmanagement.entity.abstracts.Instructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "courses")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "students"})
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "course_code")
+    private String courseCode;
+    @Column(name = "credit")
+    private int credit;
+
+    @ManyToMany
+    private List<Student> students;
+
+    @ManyToOne
+    private Instructor instructor;
+
+
+}
